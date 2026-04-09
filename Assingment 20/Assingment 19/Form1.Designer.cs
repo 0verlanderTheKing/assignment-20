e1e3ec173d6828b87fc14bb44e42b2f4899db10c
Jaden Whitt <jadenmike31@gmail.com>
2026-04-09 19:14:34 -0400

    Add files via upload

---
+++ Assingment 20/Assingment 19/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Assingment_19
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.total = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(459, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Morse code: 1 space between letters, 3 spaces between words.";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(35, 105);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(323, 22);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // total
+            // 
+            this.total.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.total.Location = new System.Drawing.Point(238, 161);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(520, 23);
+            this.total.TabIndex = 2;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(35, 154);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(138, 23);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "Convert to Text";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(191, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Text:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 250);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Morse Code Decoder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label total;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Assingment 20/Assingment 19/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Assingment_19
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, char> morseToText;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMorseCode();
+        }
+
+        private void InitializeMorseCode()
+        {
+            Dictionary<char, string> wordBank = new Dictionary<char, string>()
+            {
+                { '.', ".-.-.-" },
+                { '0', "-----" },
+                { '1', ".----" },
+                { '2', "..---" },
+                { '3', "...--" },
+                { '4', "....-" },
+                { '5', "....." },
+                { '6', "-...." },
+                { '7', "--..." },
+                { '8', "---.." },
+                { '9', "----." },
+                { 'A', ".-" },
+                { 'B', "-..." },
+                { 'C', "-.-." },
+                { 'D', "-.." },
+                { 'E', "." },
+                { 'F', "..-." },
+                { 'G', "--." },
+                { 'H', "...." },
+                { 'I', ".." },
+                { 'J', ".---" },
+                { 'K', "-.-" },
+                { 'L', ".-.." },
+                { 'M', "--" },
+                { 'N', "-." },
+                { 'O', "---" },
+                { 'P', ".--." },
+                { 'Q', "--.-" },
+                { 'R', ".-." },
+                { 'S', "..." },
+                { 'T', "-" },
+                { 'U', "..-" },
+                { 'V', "...-" },
+                { 'W', ".--" },
+                { 'X', "-..-" },
+                { 'Y', "-.--" },
+                { 'Z', "--.." }
+            };
+
+            morseToText = new Dictionary<string, char>(StringComparer.OrdinalIgnoreCase);
+
+            foreach (KeyValuePair<char, string> entry in wordBank)
+            {
+                morseToText[entry.Value] = entry.Key;
+            }
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string input = inputTextBox.Text.Trim();
+                total.Text = TranslateToText(input);
+            }
+            catch (Exception ex)
+            {
+                total.Text = string.Empty;
+                MessageBox.Show(ex.Message, "Invalid Morse Code", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string TranslateToText(string morseCode)
+        {
+            if (string.IsNullOrWhiteSpace(morseCode))
+            {
+                return string.Empty;
+            }
+
+            ValidateInputCharacters(morseCode);
+
+            string normalizedInput = NormalizeWhitespace(morseCode.Trim());
+            List<string> words = SplitWords(normalizedInput);
+            StringBuilder textResult = new StringBuilder();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                string[] letters = words[i].Split(' ');
+
+                foreach (string letter in letters)
+                {
+                    if (!morseToText.ContainsKey(letter))
+                    {
+                        MessageBox.Show  ("Invalid Input");
+                    }
+
+                    textResult.Append(morseToText[letter]);
+                }
+
+                if (i < words.Count - 1)
+                {
+                    textResult.Append(' ');
+                }
+            }
+
+            return textResult.ToString();
+        }
+
+        private string NormalizeWhitespace(string input)
+        {
+            StringBuilder normalized = new StringBuilder();
+
+            foreach (char character in input)
+            {
+                if (character == '\t' || character == '\r' || character == '\n')
+                {
+                    normalized.Append(' ');
+                }
+                else
+                {
+                    normalized.Append(character);
+                }
+            }
+
+            return normalized.ToString();
+        }
+
+        private List<string> SplitWords(string morseCode)
+        {
+            List<string> words = new List<string>();
+            StringBuilder currentWord = new StringBuilder();
+            int spaceCount = 0;
+
+            foreach (char character in morseCode)
+            {
+                if (character == ' ')
+                {
+                    spaceCount = spaceCount + 1;
+
+                    if (spaceCount >= 1)
+                    {
+                    }
+
+                    continue;
+                }
+
+                if (spaceCount == 2)
+                {
+                }
+
+                if (spaceCount == 3)
+                {
+                    if (currentWord.Length == 0)
+                    {
+                    }
+
+                    words.Add(currentWord.ToString());
+                    currentWord.Clear();
+                }
+                else if (spaceCount == 1)
+                {
+                    if (currentWord.Length == 0 || currentWord[currentWord.Length - 1] == ' ')
+                    {
+                    }
+
+                    currentWord.Append(' ');
+                }
+
+                currentWord.Append(character);
+                spaceCount = 0;
+            }
+
+            if (spaceCount == 2)
+            {
+                MessageBox.Show("Invalid Input");
+            }
+
+            if (spaceCount == 3)
+            {
+                MessageBox.Show("Invalid Input");
+            }
+
+            if (currentWord.Length > 0)
+            {
+                words.Add(currentWord.ToString());
+            }
+
+            return words;
+        }
+
+        private void ValidateInputCharacters(string morseCode)
+        {
+            foreach (char character in morseCode)
+            {
+                if (character == '.' || character == '-' || character == ' ' || character == '\t' || character == '\r' || character == '\n')
+                {
+                    continue;
+                }
+
+                if (char.IsLetterOrDigit(character))
+                {
+                    MessageBox.Show("Invalid Input");
+                }
+
+                MessageBox.Show("Invalid Input");
+            }
+        }
+    }
+}
